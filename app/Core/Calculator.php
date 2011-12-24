9030e422833147f34aff498819728954ea41bbd3
Volodymyr Dubyna <vdubyna@speroteck.com>
2011-12-24 02:43:41 +0200

    Correct pmd task

--- app/Core/Calculator.php
+++ app/Core/Calculator.php
@@ -2,11 +2,21 @@
 
 class Core_Calculator
 {
+    /**
+     * @param int $a
+     * @param int $b
+     * @return float
+     */
     public function sum($a, $b)
     {
         return $a + $b;
     }
 
+    /**
+     * @param int $a
+     * @param int $b
+     * @return float
+     */
     public function division($a, $b)
     {
         return $a / $b;
